1f1e8d3e872e12ed819d7d5ebe3cd1e8ba29d977
Agboola Akorede <akoredeagboola23@gmail.com>
2025-04-14 05:51:43 +0100

    chore: include view engine

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { corsOptions, config } from './configs/index.js';
 import { router } from './src/routes/index.js';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors(corsOptions));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views')); 
+
+app.use(cors(corsOptions));
 app.use(morgan('tiny'));
 app.disable('x-powered-by');
-// app.use(addRequestCountToLocals, addReturnCountToLocals, addWasteCountToLocals);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(router);
-
-
-
 
 const port = config.port || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
