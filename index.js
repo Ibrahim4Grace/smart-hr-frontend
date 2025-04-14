fcef3f3ec1edda2b2514cd5ad7ff8e5c2791d4a7
Agboola Akorede <akoredeagboola23@gmail.com>
2025-04-14 06:27:30 +0100

    chores: modified

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 
